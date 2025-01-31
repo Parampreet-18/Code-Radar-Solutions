2b9baf360ae0188ed31ec5438219f50d9ccac771
Parampreet-18 <parampreetsaini2343@gmail.com>
2025-01-31 09:09:38 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,7 +6,7 @@ int main() {
     {
         printf("Vowel");
     }
-    else if(a>='a'&& a<='z'){
+    else if(a>='a'&&a<='z'||a>='A'&&<='Z'){
         printf("Consonant");
     }
     else if(a>=0 && a<= 9){
