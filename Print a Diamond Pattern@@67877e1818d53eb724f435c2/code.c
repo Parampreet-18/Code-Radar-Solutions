b59cc7c624aa2709c6ceefa5ec6e4184535c439d
Parampreet-18 <parampreetsaini2343@gmail.com>
2025-03-16 13:55:36 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -4,23 +4,23 @@ int main() {
     int a;
     scanf("%d", &a);
 
-    // Upper Part (Mirrored Right-Angled Triangle)
+    // Upper Part (Increasing)
     for (int i = 1; i <= a; i += 2) {
-        for (int j = 1; j <= (a - i) / 2; j++) { // Print spaces
+        for (int j = 0; j < (a - i) / 2; j++) { // Print leading spaces
             printf(" ");
         }
-        for (int k = 1; k <= i; k++) { // Print stars
+        for (int k = 0; k < i; k++) { // Print stars
             printf("*");
         }
         printf("\n");
     }
 
-    // Lower Part (Inverted Mirrored Right-Angled Triangle)
+    // Lower Part (Decreasing)
     for (int i = a - 2; i >= 1; i -= 2) {
-        for (int j = 1; j <= (a - i) / 2; j++) { // Print spaces
+        for (int j = 0; j < (a - i) / 2; j++) { // Print leading spaces
             printf(" ");
         }
-        for (int k = 1; k <= i; k++) { // Print stars
+        for (int k = 0; k < i; k++) { // Print stars
             printf("*");
         }
         printf("\n");
