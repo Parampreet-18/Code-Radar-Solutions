303c690369d54ad50a3f39a6793cf95d80f3536f
Parampreet-18 <parampreetsaini2343@gmail.com>
2025-03-29 12:35:09 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,20 +1,20 @@
-// Your code here...#include <stdio.h>
-void selectionSort(int arr[], int n) {
-    int i, j, temp;
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+void selectionSort(int arr[],int n) {
+    int i,j,temp;
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-i-1;j++){
+            if (arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
-            
         }
     }
+    
 }
-void printArray(int arr[], int size) {
+void printArray(int size, int arr[]) {
     int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
+    for(i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
     printf("\n");
 }
